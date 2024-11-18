cb86e42ecc2a1ae9638dc034086da5fb76a4a18a
William Alves <williamcostacardoso94@gmail.com>
2024-11-17 22:48:46 -0300

    feat: test

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ export default function Home() {
     <div>
       <header className="bg-zinc-500 p-4">HEADER pokemon</header>
       <main>MAIN content here</main>
+      <div>test</div>
       <footer className="bg-zinc-500 p-4">FOOTER all rights reserved</footer>
     </div>
   );
