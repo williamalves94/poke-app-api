cb19f402536ef031ccef5a3587115c302bd70c3d
William Alves <williamcostacardoso94@gmail.com>
2024-11-17 22:47:03 -0300

    adicionando components iniciais

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <header className="bg-zinc-500 p-4">HEADER pokemon</header>
-      <main>MAIN content</main>
+      <main>MAIN content here</main>
       <footer className="bg-zinc-500 p-4">FOOTER all rights reserved</footer>
     </div>
   );
